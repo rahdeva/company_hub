1c4cc784dc44b25e2907dcee33f3e02f93534650
Ngurah Deva <wirandanaputra@gmail.com>
2023-04-15 11:07:51 +0800

    finish login UI

--- lib/feature/login/login_controller.dart
+++ lib/feature/login/login_controller.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/widgets.dart';
+import 'package:company_hub/resources/resources.dart';
+import 'package:company_hub/routes/page_names.dart';
+import 'package:company_hub/utills/widget/snackbar/snackbar_widget.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:get/get.dart';
 
@@ -6,4 +9,24 @@ enum LoginMethod { username, google, microsoft }
 
 class LoginController extends GetxController {
   final GlobalKey<FormBuilderState> formKey = GlobalKey<FormBuilderState>();
+
+  bool isObscure = true;
+
+  void loginWithEmail(String username, String password){
+    if(
+      username == "admin" &&
+      password == "12345"
+    ){
+      Get.offAllNamed(PageName.NAVTAB);
+    } else{
+      SnackbarWidget.defaultSnackbar(
+        icon: const Icon(
+          Icons.cancel,
+          color: AppColors.red,
+        ),
+        subtitle: "Username atau Password salah",
+        title: "Login Gagal!"
+      );
+    }
+  }
 }
--- lib/feature/login/login_page.dart
+++ lib/feature/login/login_page.dart
@@ -2,11 +2,13 @@ import 'package:company_hub/resources/resources.dart';
 import 'package:company_hub/routes/page_names.dart';
 import 'package:company_hub/utills/widget/button/primary_button.dart';
 import 'package:company_hub/utills/widget/colored_status_bar.dart';
-import 'package:flutter/gestures.dart';
+import 'package:company_hub/utills/widget/forms/label_form_widget.dart';
+import 'package:company_hub/utills/widget/forms/text_field_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:get/get.dart';
 import '/utills/helper/validator.dart';
+import 'package:sizer/sizer.dart';
 
 import 'login_controller.dart';
 
@@ -30,85 +32,91 @@ class LoginPage extends StatelessWidget {
                   margin: const EdgeInsets.symmetric(horizontal: 16.0),
                   child: SingleChildScrollView(
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         const SizedBox(height: 80),
                         Container(
-                          width: 150,
-                          height: 150,
+                          width: 100.w - 40,
+                          height: 200,
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(8),
                             image: DecorationImage(
-                              image: AppImages.imgSCMHubLogo.image().image
+                              image: AppImages.imgCompanyhubLogo.image().image
                             )
                           ),
                         ),
-                        const SizedBox(height: 24),
-                        Text(
-                          "txt_general_scm_hub".tr,
-                          style: Theme.of(context).textTheme.displayLarge!.copyWith(
-                            fontWeight: FontWeight.w700,
-                            color: AppColors.colorPrimary
-                          ),
-                        ),
                         const SizedBox(height: 40),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            "txt_general_username".tr,
-                            style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                              color: AppColors.textColour70
-                            ),
+                        Container(
+                          padding: const EdgeInsets.all(24),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(35),
+                            color: AppColors.surface,
+                            boxShadow: [AppElevation.elevation4px]
                           ),
-                        ),
-                        const SizedBox(height: 8),
-                        FormBuilderTextField(
-                          name: 'username',
-                          decoration: InputDecoration(
-                            hintText: 'txt_general_username'.tr,
-                          ),
-                          validator: Validator.required(),
-                          keyboardType: TextInputType.emailAddress,
-                          maxLines: 1,
-                          onChanged: (text) {
-                          }
-                        ),
-                        Column(
-                          children: [
-                            const SizedBox(height: 16),
-                            Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                "txt_general_password".tr,
-                                style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                                  color: AppColors.textColour70
+                          child: Column(
+                            children: [
+                              const SizedBox(height: 16),
+                              Text(
+                                "Login",
+                                style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                                  fontWeight: FontWeight.w700,
+                                  color: AppColors.colorPrimary
                                 ),
                               ),
-                            ),
-                            const SizedBox(height: 8),
-                            FormBuilderTextField(
-                              name: 'password',
-                              decoration: InputDecoration(
-                                isDense: true,
-                                hintText: 'txt_general_password'.tr,
-                                suffixIcon: IconButton(
-                                  onPressed: (){},
-                                  icon: const Icon(Icons.remove_red_eye),
-                                  color: AppColors.colorSecondary,
-                                )
+                              const SizedBox(height: 32),
+                              const LabelFormWidget(
+                                labelText: "Username",
                               ),
-                              validator: Validator.required(),
-                              obscureText: true,
-                              maxLines: 1,
-                            ),
-                            const SizedBox(height: 24),
-                            PrimaryButton(
-                              title: 'txt_button_sign_in'.tr,
-                              onPressed: () async {
-                                Get.offAllNamed(PageName.NAVTAB);
-                              }
-                            ),
-                          ],
+                              TextFieldWidget(
+                                name: 'username',
+                                hintText: 'Username',
+                                validator: Validator.required(),
+                              ),
+                              const SizedBox(height: 16),
+                              const LabelFormWidget(
+                                labelText: "Password",
+                              ),
+                              TextFieldWidget(
+                                name: 'password',
+                                hintText: 'Password',
+                                obsecure: controller.isObscure,
+                                validator: Validator.required(),
+                                keyboardType: TextInputType.text,
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    controller.isObscure = !controller.isObscure;
+                                    controller.update();
+                                  },
+                                  icon: controller.isObscure
+                                    ? const Icon(
+                                        Icons.visibility_outlined,
+                                        size: 20,
+                                        color: AppColors.colorPrimary,
+                                      )
+                                    : const Icon(
+                                        Icons.visibility_off_outlined,
+                                        size: 20,
+                                        color: AppColors.colorPrimary,
+                                      ),
+                                ), 
+                              ),
+                              const SizedBox(height: 24),
+                              PrimaryButton(
+                                title: 'Sign in',
+                                onPressed: () async {
+                                  if (
+                                    controller.formKey.currentState != null && 
+                                    controller.formKey.currentState!.saveAndValidate()
+                                  ) {
+                                    controller.loginWithEmail(
+                                      controller.formKey.currentState!.fields["username"]!.value,
+                                      controller.formKey.currentState!.fields["password"]!.value,
+                                    );
+                                  }
+                                }
+                              ),
+                            ],
+                          ),
                         ),
                       ],
                     ),
